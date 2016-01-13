c41cf068d0cdcd4dfeedc3564b45e6da256d38e6
jainabafadiga <jainabafadiga@claremontihs.org>
2016-01-13 23:21:11 +0000

    final commit

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,16 +1,73 @@
 var userPoint = 0;
 var aiPoint = 0;
 
-// This function returns the selection of the computer
+// Randomly picks either "rock", "paper", or "scissors"
 function getAISelection() {
-    //TODO: randomly choose between 'rock', 'paper', or 'scissors'
+  var randomNumber = Math.ceil(Math.random()*3);
+  console.log(randomNumber);
+  if(randomNumber == 1) {
+    return "rock";
+  } 
+  if(randomNumber==2){
+    return "paper";
+  }
+ if(randomNumber==3){
+   return "scissor";
+ }
 }
 
+
 // This function picks the winner
-function pickWinner(userValue, aiValue) {
+function pickWinner(uservalue, aivalue) {
     //TODO: pick the correct winner: user or ai
     //TODO: Add one point for the winner
+if(uservalue =="paper" && aivalue == "scissor") {
+aiPoint=aiPoint+1
+       return "computer";
+}       
+
+else if(uservalue =="paper" && aivalue == "rock") {
+    userPoint=userPoint+1
+            return "user";
 }
+
+else if(uservalue =="scissor" && aivalue =="rock") {
+   aiPoint=aiPoint=1
+     return "computer";
+     
+}
+
+else if(uservalue =="rock" && aivalue =="rock") {
+     return "draw";
+}
+
+else if(uservalue =="rock" && aivalue =="scissor") {
+    userPoint=userPoint+1
+         return "user";
+}
+
+else if (uservalue =="scissor" && aivalue =="scissor") {
+          return "draw";
+}
+
+else if (uservalue =="scissor" && aivalue =="paper") {
+    userPoint=userPoint=1
+     return "user" ;
+}
+
+else if (uservalue =="scissor" && aivalue =="paper") {
+    userPoint=userPoint+1
+           return "user";
+}
+
+else if(uservalue =="paper" && aivalue =="paper") {
+    return "draw";
+}
+
+else if(user =="paper" && aivalue =="rock") {
+    userPoint=userPoint+1
+        return "user";
+}}
 
 // This function sets the scoreboard with the correct points
 function setScore() {
@@ -22,8 +79,8 @@ function evaluate(evt) {
     var userValue = evt.target.getAttribute('id');
     var aiValue = getAISelection();
 
-    var winner = pickWinner(userValue, aiValue);
-
+    var winner = pickWinner(userValue, aiValue)
+    
     if ( 'user' === winner ) {
         $('#message').delay(50).text('You have won!, Click a box to play again');
     } else if ( winner === 'draw' ) {
